d710dd76cb08c2b82abf38d6d5b2526d8d0e7964
Roman Rudenko <ruroma18@gmail.com>
2022-09-07 10:35:10 +0300

    Add FreshIdeas cmponent

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.scss';
+import FreshIdeas from './components/FreshIdeas/FreshIdeas';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <FreshIdeas />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/FreshIdeas/FreshIdeas.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./FreshIdeas.module.scss";
+
+const FreshIdeas = () => {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.heading}>Choose fresh ideas to do</h2>
+
+    </section>
+  );
+};
+
+export default FreshIdeas;
