e688326b456eb30f4dcf02add738c4fd4ed62a77
Roman Rudenko <ruroma18@gmail.com>
2022-09-08 21:09:47 +0300

    Add styles to IdeaCard

--- client/src/components/IdeaCard/IdeaCard.jsx
+++ client/src/components/IdeaCard/IdeaCard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import cx from 'classnames';
 import { Card, Typography } from "@mui/material";
 import styles from './IdeaCard.module.scss'
 
-const IdeaCard = ({task, type}) => {
+const IdeaCard = ({task, type, isCurrent}) => {
+
+  const categoryStyle = cx( {
+    [styles.category] : true,
+    [styles.hiddenCategory] : isCurrent === false
+  })
   return (
     <Card className={styles.card}>
       <Typography className={styles.task}>{task}</Typography>
-      <Typography className={styles.category}>{type}</Typography>
+      <Typography className={categoryStyle}>{type}</Typography>
     </Card>
   );
 };
