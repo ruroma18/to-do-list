0c900a675f55b94e75732401e857316f51e5e2ec
Roman Rudenko <ruroma18@gmail.com>
2022-09-09 11:18:31 +0300

    Add slider pagination

--- client/src/components/IdeasList/IdeasList.jsx
+++ client/src/components/IdeasList/IdeasList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import IdeaCard from "../IdeaCard/IdeaCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -7,11 +7,14 @@ import styles from "./IdeasList.module.scss";
 import "./slider.css";
 
 const IdeasList = ({ myIdeas, setMyIdeas }) => {
+
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(1);
   
   const changeVisability = (current) => {
     const newMyIdeas = myIdeas.map((idea, index) => ({
       ...idea,
-      isCurrent: index === current ? idea.isCurrent = true : idea.isCurrent = false,
+      isCurrent:
+        index === current ? (idea.isCurrent = true) : (idea.isCurrent = false),
     }));
     return setMyIdeas(newMyIdeas);
   };
@@ -23,7 +26,7 @@ const IdeasList = ({ myIdeas, setMyIdeas }) => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    afterChange: (current) => changeVisability(current),
+    afterChange: (current) => [changeVisability(current), setCurrentSlideIndex(current + 1)],
   };
 
   return (
@@ -31,18 +34,21 @@ const IdeasList = ({ myIdeas, setMyIdeas }) => {
       <h2 className={styles.heading}>Idea in my list</h2>
       <div className={styles.sliderWrapper}>
         {myIdeas.length === 0 ? (
-          <div>No ideas in your list</div>
+          <div className={styles.noCards}>Add ideas in your list</div>
         ) : (
-          <Slider {...slideSetting}>
-            {myIdeas.map((idea) => (
-              <IdeaCard
-                key={idea.key}
-                task={idea.activity}
-                type={idea.type}
-                isCurrent={idea.isCurrent}
-              />
-            ))}
-          </Slider>
+          <>
+            <Slider {...slideSetting}>
+              {myIdeas.map((idea) => (
+                <IdeaCard
+                  key={idea.key}
+                  task={idea.activity}
+                  type={idea.type}
+                  isCurrent={idea.isCurrent}
+                />
+              ))}
+            </Slider>
+            <div className={styles.sliderPagination}>{` ${currentSlideIndex} / ${myIdeas.length}`}</div>
+          </>
         )}
       </div>
     </section>
