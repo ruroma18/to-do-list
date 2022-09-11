d9349763521790839bf6625a62ddfef5968048a8
Roman Rudenko <ruroma18@gmail.com>
2022-09-11 20:08:53 +0300

    Refactoring IdeaList

---
+++ client/src/components/Ideas/IdeaSlider/IdeaSlider.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./slider.css";
+import IdeaCard from "../IdeaCard/IdeaCard";
+
+
+const IdeaSlider = ({setMyIdeas, myIdeas, setTaskDone}) => {
+
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(1);
+
+  const changeVisability = (current) => {
+    const newMyIdeas = myIdeas.map((idea, index) => ({
+      ...idea,
+      isCurrent:
+        index === current ? (idea.isCurrent = true) : (idea.isCurrent = false),
+    }));
+    return setMyIdeas(newMyIdeas);
+  };
+
+  const slideSetting = {
+    centerMode: true,
+    infinite: false,
+    centerPadding: "33%",
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    afterChange: (current) => [
+      changeVisability(current),
+      setCurrentSlideIndex(current + 1),
+    ],
+  };
+
+  return (
+    <div>
+      <>
+        <Slider {...slideSetting}>
+          {myIdeas.map((idea) =>
+            idea.isDone === false ? (
+              <div key={idea.key} onClick={() => setTaskDone(idea.key)}>
+                <IdeaCard
+                  task={idea.activity}
+                  type={idea.type}
+                  isCurrent={idea.isCurrent}
+                />
+              </div>
+            ) : (
+              false
+            )
+          )}
+        </Slider>
+        <div style={{textAlign: "center"}}>{` ${
+          myIdeas.length === 1 ? 1 : currentSlideIndex
+        } / ${myIdeas.length}`}</div>
+      </>
+    </div>
+  );
+};
+
+export default IdeaSlider;
--- client/src/components/Ideas/IdeasList/IdeasList.jsx
+++ client/src/components/Ideas/IdeasList/IdeasList.jsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
-import IdeaCard from "../IdeaCard/IdeaCard";
+import React from "react";
 import rewritreLocalStorage from "../../../utils/rewriteLocalStorage";
 import setDataToLocalStorage from "../../../utils/writeLocalStorage";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import IdeaSlider from "../IdeaSlider/IdeaSlider";
 import styles from "./IdeasList.module.scss";
-import "./slider.css";
+
 
 const IdeasList = ({
   myIdeas,
@@ -15,7 +12,7 @@ const IdeasList = ({
   setAchievements,
   setDoneIdeas,
 }) => {
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(1);
+  
 
   const countAchievements = (idea) => {
     const existingAchievement = achievements.find(
@@ -59,28 +56,7 @@ const IdeasList = ({
       return myIdeas;
     });
   };
-
-  const changeVisability = (current) => {
-    const newMyIdeas = myIdeas.map((idea, index) => ({
-      ...idea,
-      isCurrent:
-        index === current ? (idea.isCurrent = true) : (idea.isCurrent = false),
-    }));
-    return setMyIdeas(newMyIdeas);
-  };
-
-  const slideSetting = {
-    centerMode: true,
-    infinite: false,
-    centerPadding: "33%",
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    afterChange: (current) => [
-      changeVisability(current),
-      setCurrentSlideIndex(current + 1),
-    ],
-  };
+  
 
   return (
     <section className={styles.container}>
@@ -89,26 +65,7 @@ const IdeasList = ({
         {myIdeas.length === 0 ? (
           <div className={styles.noCards}>Add ideas in your list</div>
         ) : (
-          <>
-            <Slider {...slideSetting}>
-              {myIdeas.map((idea) =>
-                idea.isDone === false ? (
-                  <div key={idea.key} onClick={() => setTaskDone(idea.key)}>
-                    <IdeaCard
-                      task={idea.activity}
-                      type={idea.type}
-                      isCurrent={idea.isCurrent}
-                    />
-                  </div>
-                ) : (
-                  false
-                )
-              )}
-            </Slider>
-            <div className={styles.sliderPagination}>{` ${
-              myIdeas.length === 1 ? 1 : currentSlideIndex
-            } / ${myIdeas.length}`}</div>
-          </>
+          <IdeaSlider setMyIdeas={setMyIdeas} myIdeas={myIdeas} setTaskDone={setTaskDone} />          
         )}
       </div>
     </section>
