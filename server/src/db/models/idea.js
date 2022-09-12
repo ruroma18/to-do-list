e0e82d736da3a15afad8694296eec7c62b34fee8
Roman Rudenko <ruroma18@gmail.com>
2022-09-12 16:47:04 +0300

    Add createdAt to ideaSchema

--- server/src/db/models/idea.js
+++ server/src/db/models/idea.js
@@ -3,7 +3,8 @@ const {Schema, model} = require('../index');
 const ideaSchema = new Schema ({
   myIdeas: {type: Array, required: true},
   doneIdeas: {type: Array, required: true},
-  achievements: {type: Array, required: true}
+  achievements: {type: Array, required: true},
+  createdAt: {type: Date, default: Date.now}
 })
 
 const Idea = model('Idea', ideaSchema);
